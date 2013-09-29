c22722f687bffeebde9eabd8d294297722dec59e
ice-ice <kwge107@gmail.com>
2013-09-29 19:58:17 -0400

    Update GasStation.java

--- Source Code/GasStation.java
+++ Source Code/GasStation.java
@@ -1,6 +1,6 @@
 public int canCompleteCircuit(int[] gas, int[] cost) {
 	// Note: The Solution object is instantiated only once and is reused by each test case.
-	
+
 	int cycle = gas.length, sum = 0, total = 0, start = 0;
 	for (int i = 0; i < cycle; i++) {
 	    sum += gas[i] - cost[i];
