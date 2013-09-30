11ca76125a5ed017f9b2cdbc8522ec60707b557f
ice-ice <kwge107@gmail.com>
2013-09-29 21:47:14 -0400

    Update N_Queens.java

--- Source Code/N_Queens.java
+++ Source Code/N_Queens.java
@@ -1,14 +1,14 @@
-public ArrayList<String[]> solveNQueens(int n) {
-  // Start typing your Java solution below
-  // DO NOT write main() function
-  
-  ArrayList<String[]> chessLists = new ArrayList<String[]>();
-  ArrayList<int[]> lists = new ArrayList<int[]>();
-  int[] columns = new int[n];
-  
-  placeQueens(columns, n, 0, lists);
-  
-  for (int i = 0; i < lists.size(); i++) {
+    public ArrayList<String[]> solveNQueens(int n) {
+    // Start typing your Java solution below
+    // DO NOT write main() function
+    
+    ArrayList<String[]> chessLists = new ArrayList<String[]>();
+    ArrayList<int[]> lists = new ArrayList<int[]>();
+    int[] columns = new int[n];
+    
+    placeQueens(columns, n, 0, lists);
+    
+    for (int i = 0; i < lists.size(); i++) {
       String[] chess = new String[n];
       for (int j = 0; j < n; j++) {
           String s = "";
@@ -22,33 +22,33 @@ public ArrayList<String[]> solveNQueens(int n) {
           chess[j] = s;
       }
       chessLists.add(chess);
-  }
-  
-  return chessLists;
-}
-
-public void placeQueens(int[] columns, int n, int rows, ArrayList<int[]> lists) {
-  if (rows >= n) {
+    }
+    
+    return chessLists;
+    }
+    
+    public void placeQueens(int[] columns, int n, int rows, ArrayList<int[]> lists) {
+    if (rows >= n) {
       int[] result = new int[n];
-  	for (int j = 0; j < n; j++) {
-  		result[j] = columns[j];
-  	}
+      for (int j = 0; j < n; j++) {
+      	result[j] = columns[j];
+      }
       lists.add(result);
       return;
-  }
-  
-  for (int i = 0; i < n; i++) {
+    }
+    
+    for (int i = 0; i < n; i++) {
       columns[rows] = i;
       if (checkValid(columns, rows)) {
           placeQueens(columns, n, rows + 1, lists);
       }
-  }
-}
-
-public boolean checkValid(int[] columns, int row2) {
-  int row1 = 0;
-  int column2 = columns[row2];
-  for (; row1 < row2; row1++) {
+    }
+    }
+    
+    public boolean checkValid(int[] columns, int row2) {
+    int row1 = 0;
+    int column2 = columns[row2];
+    for (; row1 < row2; row1++) {
       int column1 = columns[row1];
       if (column1 == column2) {
           return false;
@@ -56,6 +56,6 @@ public boolean checkValid(int[] columns, int row2) {
       if (Math.abs(row1 - row2) == Math.abs(column1 - column2)) {
           return false;
       }
-  }
-  return true;
-}
+    }
+    return true;
+    }
